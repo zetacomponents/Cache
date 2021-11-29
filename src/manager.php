8eb3131fbc6e221c1f40300eb2532745a63aa9d5
Derick Rethans <github@derickrethans.nl>
2021-11-29 14:44:42 +0000

    Make sure to have default values for non-optional arguments

--- src/manager.php
+++ src/manager.php
@@ -9,9 +9,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -209,7 +209,7 @@ class ezcCacheManager
      *         If the given storage class does not exist or is no subclass of
      *         ezcCacheStorage.
      */
-    public static function createCache( $id, $location = null, $storageClass, $options = array() )
+    public static function createCache( $id, $location = null, $storageClass = 'ezcCacheStorage', $options = array() )
     {
         // BC for missing location. The location should not be missing.
         if ( $location !== null )
